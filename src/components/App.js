ae38410e2f2dc9d5bb1e7b131a3a0858361ccaef
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-17 23:58:54 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+     const timer = setInterval(() => {
+      setProgress(progress=>{
+        if(progress>=100){
+          clearInterval(timer)
+        }
+        else {
+          setProgress(progress+1)
+        }
+      })
+    }, 100);
+  }, [])
+
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Progress Bar</h1>
+      <div id="barOuter">
+        <div id="barInner" style={{width:`${progress}%`}}></div>
+      </div>
+      <p>{progress}%</p>
     </div>
   )
 }
